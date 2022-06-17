ddb05c392681c1e4aeb8ca112ef597b5b4689bd4
kometenstaub <83140328+kometenstaub@users.noreply.github.com>
2022-06-17 10:32:37 +0200

    fix: abort in reading mode

--- src/main.ts
+++ src/main.ts
@@ -175,6 +175,10 @@ export default class MarkGutter extends Plugin {
 					// adapted from: https://github.com/mrjackphil/obsidian-jump-to-link/issues/35#issuecomment-1085905668
 					let keyArray: string[] = [];
 					this.grabKey = (event: KeyboardEvent) => {
+						if (currentLeaf.getMode() === 'preview') {
+							return;
+						}
+
 						// empty array if Esc
 						if (event.key === 'Escape') {
 							keyArray = [];
